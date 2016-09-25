0bdbd985711dad941722a418399e0ba08949d055
David Jaramillo <david.jaramillo.c@gmail.com>
2016-09-25 19:18:48 -0300

    fix order library and verson

--- public/ToOpcodes.js
+++ public/ToOpcodes.js
@@ -2,6 +2,19 @@
 	From hex to opcodes
 */
 
+
+function processsHexcode (_hexcode)
+{
+    //processs _hexcode
+    return [{
+    opcode:"PUSH",hexCode:"0x60", data:"0x01" ,"class":"push"
+    },
+    {
+    opcode:"SLOAD",hexCode:"0x54", "class":"sload"
+    }
+    ];
+}
+
 $("#hexcode").bind("input propertychange", function() {
 
     $("#opcodes").css("color", "red");
