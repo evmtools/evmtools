d52339cc660a41ce747a08167be94c555f4b1f64
David Jaramillo <david.jaramillo.c@gmail.com>
2016-09-26 11:51:43 -0300

    implement parse and library

--- public/OpCodes.js
+++ public/OpCodes.js
@@ -1,22 +1,81 @@
-import _ from 'lodash';
-import * as opcodes from './OpCodesList';
 
-const instructionToName = (() => {
+
+var instructionToName =(function ()  {
 	return _.zipObject(_.values(opcodes), _.keys(opcodes));
 })();
 
-export function getName (i) {
+var inst ={
+
+
+"getName": function  (i) {
 	return instructionToName[i];
 }
 
-export function getCode (name) {
+,"getCode": function  (name) {
 	return opcodes[name];
 }
 
-export function isPush (i) {
+,"isPush": function  (i) {
 	return i <= opcodes.PUSH32 && i >= opcodes.PUSH1;
 }
 
-export function getPushBytes (i) {
+,"getPushBytes":function  (i) {
 	return i - opcodes.PUSH1 + 1;
 }
+
+}
+
+
+var asHex2=function (i) {
+
+	return '0x'+i.toUpperCase();
+}
+
+var asHex =function asHex(i) {
+	let x = i.toString(16);
+	if (x.length < 2) {
+		return '0x0'+x;
+	}
+	return '0x'+x;
+}
+
+var padName =function (n) {
+	return _.padEnd(n, 7, '\u00a0');
+}
+
+
+
+var	parseCode=function(bytes) {
+		var pc = 0;
+		var result = [];
+		while (pc < bytes.length) {
+			var i = parseInt(bytes[pc], 16);
+			var name = inst.getName(i);
+			if (!name) {
+				var e = new Error('Unknown instruction: '+bytes[pc]+' at '+pc+'.');
+				e.pos = pc;
+				throw e;
+			}
+			var instruction = {
+				pc: pc,
+				name: inst.getName(i),
+				opcode: i,
+				param: ''
+			};
+			if (inst.isPush(i)) {
+				var b = inst.getPushBytes(i);
+				var info=bytes.slice(pc + 1, pc + b + 1).join('');
+				var word = info;// parseInt(info, 16);
+				//--console.log("info: "+info)
+				//--console.log("word: "+word)
+				///console.log("word: "+ asHex(word))
+				instruction.param = asHex2(word);
+				pc += b;
+			}
+			result.push(instruction);
+			pc += 1;
+		}
+
+
+		return result;
+	}
--- public/ToOpcodes.js
+++ public/ToOpcodes.js
@@ -2,9 +2,31 @@
 	From hex to opcodes
 */
 
+var executeParseCode=function(_code)
+{
+	var code = _code.replace(/^0x/, '').replace(/\s/g, '');
+		// Try to parse code
+		var bytes = _.chunk(code, 2).map((x) => x.join(''));
 
+		return parseCode(bytes);
+}
+
+/*
+//example
+
+var code= '0x6001600054016000556000600060006000600073bbbf5374fce5edbc8e2a8697c15331677e6ebf0b610401600054046001036127105a0302f16001556103e860005402600101600255';
+processsHexcode(code);
+
+*/
 function processsHexcode (_hexcode)
 {
+
+    var code = _hexcode.replace(/^0x/, '').replace(/\s/g, '');
+    // Try to parse code
+    var bytes = _.chunk(code, 2).map((x) => x.join(''));
+
+    return parseCode(bytes);
+/*
     //processs _hexcode
     return [{
     opcode:"PUSH",hexCode:"0x60", data:"0x01" ,"class":"push"
@@ -12,7 +34,7 @@ function processsHexcode (_hexcode)
     {
     opcode:"SLOAD",hexCode:"0x54", "class":"sload"
     }
-    ];
+    ];*/
 }
 
 $("#hexcode").bind("input propertychange", function() {
